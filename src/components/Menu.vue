45d97b73a6a350b85f499023a1dabaf1f74262ac
llsq1996 <2669052302@qq.com>
2018-11-16 15:35:16 +0800

    init

---
+++ src/components/Menu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+  <el-menu
+    class="el-menu-vertical-demo"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    @open="handleOpen"
+    @close="handleClose">
+    <el-submenu index="1">
+      <template slot="title">
+        <i class="el-icon-menu"></i>
+        <span>导航一</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+        <el-menu-item index="1-3">选项3</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+  </el-menu>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Menu.vue',
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
